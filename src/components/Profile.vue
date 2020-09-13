b948ef6af695aaadc23d9d3b518ff24dbcf41a28
Hlabhayh <66203720+Hlabhayh@users.noreply.github.com>
2020-09-13 20:55:33 +0200

    profile component

---
+++ src/components/Profile.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="user-head">
+    <a class="inbox-avatar" href="javascript:;">
+      <img width="64" hieght="60" src="../assets/avatar.jpg"/>
+    </a>
+    <div class="user-name">
+      <h5>
+        <a href="#">{{ name }}</a>
+      </h5>
+      <span>
+        <a href="#">{{ email }}</a>
+      </span>
+    </div>
+    <a class="mail-dropdown pull-right" href="javascript:;">
+      <i class="fa fa-chevron-down"></i>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Profile",
+  props: {
+    name: String,
+    email: String,
+    avatar: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style >
+.user-head .inbox-avatar {
+  float: left;
+  width: 65px;
+}
+.user-head .inbox-avatar img {
+  border-radius: 4px;
+}
+.user-head .user-name {
+  display: inline-block;
+  margin: 0 0 0 10px;
+}
+.user-head .user-name h5 {
+  font-size: 14px;
+  font-weight: 300;
+  margin-bottom: 0;
+  margin-top: 15px;
+}
+.user-head .user-name h5 a {
+  color: #fff;
+}
+.user-head .user-name span a {
+  color: #87e2e7;
+  font-size: 12px;
+}
+</style>
